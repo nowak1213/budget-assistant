d7c0063d9eabf52f210666cb74a05392cc8f78db
Bartłomiej Nowakowski <bartlomiejnowakowski@MacBook-Air-Bartomiej.local>
2021-06-23 01:06:08 +0200

    Initial repo creation

---
+++ src/main/java/com/nowakowski/bartlomiej/budget/assistant/BudgetAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.nowakowski.bartlomiej.budget.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nowakowski/bartlomiej/budget/assistant/BudgetAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowakowski.bartlomiej.budget.assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
